6ee144f5fb9294b36ffeb291505973bef661766f
Abdur Razzak <A.Razzak@surbanajurong.com>
2023-02-09 17:19:33 +0600

    login and sign up pages are ready

---
+++ angular-part/src/app/components/signup/signup.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  type: string = "password"
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+
+    if (this.isText) {
+      this.eyeIcon = "fa-eye";
+      this.type = "text"
+    } else {
+      this.eyeIcon = "fa-eye-slash";
+      this.type = "password"
+    }
+  }
+
+}
